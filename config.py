517bf836b195ebbb8de522f5634b212efc43fb9e
BOTZEMPIRE <chaudharyji0805@gmail.com>
2026-02-12 09:29:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN") or "PASTE_YOUR_BOT_TOKEN_HERE"
+BOT_TOKEN = os.getenv("BOT_TOKEN") or "PASTE_YOUR_BOT_TOKEN"
 
-# Jinko join karna mandatory hai
+# Force join channels
 FORCE_CHANNELS = [
     "@BotzEmpire",
     "@Yaaro_kimehfill",
     "@BotsDeal"
 ]
+
+# Admin IDs (apna Telegram ID daal)
+ADMINS = [7538572906]
+
+# Watermark / Credits
+WATERMARK = "\n\n— ✨ @YourBotUsername"
