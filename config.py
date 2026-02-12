ed674204c2e8d8550fbe7a972591615f927a7251
BOTZEMPIRE <chaudharyji0805@gmail.com>
2026-02-12 08:50:21 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN") or "PASTE_YOUR_BOT_TOKEN_HERE"
+
+# Agar future me channels/admins chahiye ho:
+ADMINS = [7538572906]
+CHANNELS = [
+    "@BotzEmpire",
+    "@Yaaro_kimehfill",
+    "@BotsDeal"
+]
